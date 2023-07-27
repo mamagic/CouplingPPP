bf681faed85b3f834023acaf24bd824b4b86cbb7
dalongyi <rrhhww0213@naver.com>
2023-07-27 13:42:57 +0900

    add files

---
+++ src/com/intheeast/collector/CSVMetadataCollector.java
@@ -0,0 +1,41 @@
+package com.intheeast.collector;
+
+import java.util.List;
+
+import com.intheeast.export.ExportMetadata;
+import com.intheeast.fetch.FetchMetadata;
+
+
+//추가합니다
+//추가합니다
+//추가합니다
+
+public class CSVMetadataCollector implements MetadataCollector {
+	
+	private FetchMetadata fetchMetadata;
+    private ExportMetadata exportMetadata;
+    
+    public CSVMetadataCollector() {
+    	
+    }
+
+	@Override
+	public void setMetadataCollector(FetchMetadata fetchMetadata, ExportMetadata exportMetadata) {
+		this.fetchMetadata = fetchMetadata;
+        this.exportMetadata = exportMetadata;
+	}
+
+	@Override
+	public void collectMetadata() {
+		List<Object> metadata = fetchMetadata.fetchMetadata();
+        exportMetadata.export(metadata);
+
+	}
+
+	@Override
+	public void print() {
+		String fetchType = fetchMetadata.getFetchType();
+		System.out.println("CSV Exporting data to" + fetchType);
+
+	}
+}
